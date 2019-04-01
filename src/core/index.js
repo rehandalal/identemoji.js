556b5d28731f00ce4a6e686f672010895902de34
Rehan Dalal <rehandalal@gmail.com>
2019-04-01 19:49:35 -0400

    Slight refactor

--- src/core/index.js
+++ src/core/index.js
@@ -14,66 +14,75 @@ export default class Identemoji {
     this.canvas.setAttribute("height", size);
   }
 
-  async draw() {
-    const hash = await getHash(this.seed);
-    const ctx = this.canvas.getContext("2d");
-    const { colors, emojis } = this.theme;
-    const s = this.size;
-
+  async drawBackground(ctx, hash) {
+    const { size } = this;
+    const { colors } = this.theme;
     const layout = parseInt(hash.substring(0, 2), 16) % 4;
-    const emoji = emojis[parseInt(hash.substring(2, 5), 16) % emojis.length];
     const color1 = colors[parseInt(hash.substring(18, 20), 16) % colors.length];
     const color2 = colors[parseInt(hash.substring(16, 18), 16) % colors.length];
 
     switch (layout) {
       case 3:
         ctx.fillStyle = color1;
-        ctx.fillRect(0, 0, s / 2, s / 2);
-        ctx.fillRect(s / 2, s / 2, s / 2, s / 2);
+        ctx.fillRect(0, 0, size / 2, size / 2);
+        ctx.fillRect(size / 2, size / 2, size / 2, size / 2);
 
         ctx.fillStyle = color2;
-        ctx.fillRect(0, s / 2, s / 2, s / 2);
-        ctx.fillRect(s / 2, 0, s / 2, s / 2);
+        ctx.fillRect(0, size / 2, size / 2, size / 2);
+        ctx.fillRect(size / 2, 0, size / 2, size / 2);
         break;
 
       case 2:
         ctx.fillStyle = color1;
-        ctx.fillRect(0, 0, s, s / 2);
+        ctx.fillRect(0, 0, s, size / 2);
 
         ctx.fillStyle = color2;
-        ctx.fillRect(0, s / 2, s, s / 2);
+        ctx.fillRect(0, size / 2, s, size / 2);
         break;
 
       case 1:
         ctx.fillStyle = color1;
-        ctx.fillRect(0, 0, s / 2, s);
+        ctx.fillRect(0, 0, size / 2, size);
 
         ctx.fillStyle = color2;
-        ctx.fillRect(s / 2, 0, s / 2, s);
+        ctx.fillRect(size / 2, 0, size / 2, size);
         break;
 
       case 0:
         ctx.fillStyle = color1;
-        ctx.fillRect(0, 0, s, s);
+        ctx.fillRect(0, 0, s, size);
         break;
     }
+  }
 
-    await new Promise(resolve => {
+  drawEmoji(ctx, hash) {
+    const { size } = this;
+    const { emojis } = this.theme;
+    const emoji = emojis[parseInt(hash.substring(2, 5), 16) % emojis.length];
+
+    return new Promise(resolve => {
       const img = new Image();
-      img.onload = function() {
+      img.onload = () => {
         ctx.shadowColor = "rgba(0, 0, 0, 0.25)";
         ctx.shadowBlur = s / 8;
         ctx.shadowOffsetX = 0;
         ctx.shadowOffsetY = 0;
         ctx.drawImage(
           img,
-          (s * 3) / 16,
-          (s * 3) / 16,
-          (s * 5) / 8,
-          (s * 5) / 8
+          (size * 3) / 16,
+          (size * 3) / 16,
+          (size * 5) / 8,
+          (size * 5) / 8
         );
       };
       img.src = svgToTinyDataUri(emoji);
     });
+  }
+
+  async draw() {
+    const hash = await getHash(this.seed);
+    const ctx = this.canvas.getContext("2d");
+    await this.drawBackground(ctx, hash);
+    await this.drawEmoji(ctx, hash);
   }
 }
