e81c97194ad4ead04e7fef216629fe4cbf84caeb
Rehan Dalal <rehandalal@gmail.com>
2019-04-02 04:42:35 -0400

    Add more layouts and better color variance testing

--- packages/core/index.js
+++ packages/core/index.js
@@ -1,12 +1,16 @@
 import svgToTinyDataUri from "mini-svg-data-uri";
 
-import { getHash } from "./utils";
+import { getDeltaE, getHash } from "./utils";
 
 export default class Identemoji {
   constructor(seed, theme, size = 96) {
     this.size = size;
     this.seed = seed;
-    this.theme = theme;
+    this.theme = {
+      layouts: [...Array(10).keys()],
+      minColorVariance: 0,
+      ...theme
+    };
 
     // Set up the canvas
     this.canvas = document.createElement("canvas");
@@ -16,21 +20,56 @@ export default class Identemoji {
 
   async drawBackground(ctx, hash) {
     const { size } = this;
-    const { colors, layouts = [...Array(8).keys()] } = this.theme;
+    const { colors, layouts, minColorVariance } = this.theme;
     const layout = layouts[parseInt(hash.substring(0, 2), 16) % layouts.length];
 
     const color = [];
-    for (let i = 0; i < 3; i++) {
+    for (let i = 0; i < 4; i++) {
       const key =
         parseInt(hash.substring(18 - i * 2, 20 - i * 2), 16) % colors.length;
       color[i] = colors[key];
 
-      if (color[i] === color[i - 1]) {
-        color[i] = colors[(key + 1) % colors.length];
+      if (color[i - 1]) {
+        let offset = 1;
+        while (
+          offset < colors.length &&
+          getDeltaE(color[i], color[i - 1]) <= minColorVariance
+        ) {
+          color[i] = colors[(key + offset) % colors.length];
+          offset++;
+        }
       }
     }
 
     switch (layout) {
+      case 9:
+        ctx.fillStyle = color[0];
+        ctx.fillRect(0, 0, size, size);
+
+        ctx.fillStyle = color[1];
+        ctx.fillRect(size / 4, 0, (size * 3) / 4, size);
+
+        ctx.fillStyle = color[2];
+        ctx.fillRect(size / 2, 0, size / 2, size);
+
+        ctx.fillStyle = color[3];
+        ctx.fillRect((size * 3) / 4, 0, size / 4, size);
+        break;
+
+      case 8:
+        ctx.fillStyle = color[0];
+        ctx.fillRect(0, 0, size, size);
+
+        ctx.fillStyle = color[1];
+        ctx.fillRect(0, size / 4, size, (size * 3) / 4);
+
+        ctx.fillStyle = color[2];
+        ctx.fillRect(0, size / 2, size, size / 2);
+
+        ctx.fillStyle = color[3];
+        ctx.fillRect(0, (size * 3) / 4, size, size / 4);
+        break;
+
       case 7:
         ctx.fillStyle = color[0];
         ctx.fillRect(0, 0, size, size);
@@ -60,22 +99,22 @@ export default class Identemoji {
         break;
 
       case 5:
-        ctx.fillStyle = color[1];
+        ctx.fillStyle = color[0];
         ctx.fillRect(0, 0, size, size);
 
-        ctx.fillStyle = color[0];
-        ctx.fillRect(0, 0, size / 3, size);
+        ctx.fillStyle = color[1];
+        ctx.fillRect(size / 3, 0, (size * 2) / 3, size);
 
         ctx.fillStyle = color[2];
         ctx.fillRect((size * 2) / 3, 0, size / 3, size);
         break;
 
       case 4:
-        ctx.fillStyle = color[1];
+        ctx.fillStyle = color[0];
         ctx.fillRect(0, 0, size, size);
 
-        ctx.fillStyle = color[0];
-        ctx.fillRect(0, 0, size, size / 3);
+        ctx.fillStyle = color[1];
+        ctx.fillRect(0, size / 3, size, (size * 2) / 3);
 
         ctx.fillStyle = color[2];
         ctx.fillRect(0, (size * 2) / 3, size, size / 3);
--- packages/core/utils.js
+++ packages/core/utils.js
@@ -1,3 +1,8 @@
+import * as rgb from "color-space/rgb";
+import * as lchab from "color-space/lchab";
+import * as xyz from "color-space/xyz";
+import deltaE from "delta-e";
+
 /**
  * Returns a SHA-1 hash of the given message.
  * @param message  The string to be hashed.
@@ -16,4 +21,32 @@ export async function getHash(message) {
   });
 
   return hexCodes.join("");
+}
+
+export function hex2RGB(hex) {
+  let v = hex;
+  if (v.startsWith("#")) {
+    v = v.substring(1);
+  }
+  if (v.length === 3) {
+    return [
+      parseInt(v.substring(0, 1), 16),
+      parseInt(v.substring(1, 2), 16),
+      parseInt(v.substring(2, 3), 16)
+    ];
+  }
+  return [
+    parseInt(v.substring(0, 2), 16),
+    parseInt(v.substring(2, 4), 16),
+    parseInt(v.substring(4, 6), 16)
+  ];
+}
+
+export function getDeltaE(hex1, hex2) {
+  const lab1 = xyz.lchab(rgb.xyz(hex2RGB(hex1)));
+  const lab2 = xyz.lchab(rgb.xyz(hex2RGB(hex2)));
+  return deltaE.getDeltaE00(
+    { L: lab1[0], A: lab1[1], B: lab1[2] },
+    { L: lab2[0], A: lab2[1], B: lab2[2] }
+  );
 }
